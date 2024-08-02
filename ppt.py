d23ed4a63da93f26b47b306967e966862e1606a5
Fuyan Yuan <33221728+TheRainstorm@users.noreply.github.com>
2024-08-02 15:57:07 +0800

    don't use simian

--- ppt.py
+++ ppt.py
@@ -19,9 +19,10 @@
 
 import json
 import sys, os, getopt, importlib
-from simian import Simian, Entity
+# from simian import Simian, Entity
 from src.kernels import Kernel
 import argparse
+from mpi4py import MPI
 
 def usage():
     print("\n[USAGE]\n\
@@ -144,6 +145,12 @@ def get_current_kernel_info(kernel_id, app_name, app_path, app_config, instructi
     return current_kernel_info
 
 def main():
+    from mpi4py import MPI
+
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+    
     parser = argparse.ArgumentParser(
         description='ppt-gpu. [MPI] For scalabilty, add mpirun call before program command:\nmpirun -np <number of processes>'
     )
@@ -256,17 +263,18 @@ def main():
     app_res_ref = None
     app_name_ = app_path.split("/")[-2]
     app_arg_ = app_path.split("/")[-1]
+    app_and_arg = f"{app_name_}/{app_arg_}"
     if args.hw_res:
         with open(args.hw_res) as fp:
             res_ref = json.load(fp)
-        app_and_arg = f"{app_name_}/{app_arg_}"
         app_res_ref = res_ref[app_and_arg]
 
     app_report_dir = app_path
     if args.report_output_dir:
         app_report_dir = os.path.join(args.report_output_dir, app_name_, app_arg_)
-        if not os.path.exists(app_report_dir):
-            os.makedirs(app_report_dir)
+        if rank==0:
+            if not os.path.exists(app_report_dir):
+                os.makedirs(app_report_dir)
     
     ##############################
     ## app configiguration file ##
@@ -294,21 +302,33 @@ def main():
         kernels_info.append(get_current_kernel_info(kernel_id, app_name, app_path, app_config, instructions_type, granularity, app_res_ref=app_res_ref, app_report_dir=app_report_dir))
     
 
-    ############################
-    # Simian Engine parameters #
-    ############################
-    simianEngine = Simian("PPT-GPU", useMPI=True, opt=False, appPath = app_report_dir, ISA=instructions_type, granularity=granularity, mpiLibName=args.libmpich_path)
-   
+    # ############################
+    # # Simian Engine parameters #
+    # ############################
+    # simianEngine = Simian("PPT-GPU", useMPI=True, opt=False, appPath = app_report_dir, ISA=instructions_type, granularity=granularity, mpiLibName=args.libmpich_path)
     gpuNode = GPUNode(gpu_configs.uarch, compute_capability.cc_configs, len(kernels_info))
         
-    for i in range (len(kernels_info)):
-        k_id = i 
-		# Add Entity and sched Event only if Hash(Entity_name_i) % MPI.size == MPI.rank
-        simianEngine.addEntity("Kernel", Kernel, k_id, len(kernels_info), gpuNode, kernels_info[i])
-        simianEngine.schedService(1, "kernel_call", None, "Kernel", k_id)
+    # for i in range (len(kernels_info)):
+    #     k_id = i 
+	# 	# Add Entity and sched Event only if Hash(Entity_name_i) % MPI.size == MPI.rank
+    #     simianEngine.addEntity("Kernel", Kernel, k_id, len(kernels_info), gpuNode, kernels_info[i])
+    #     simianEngine.schedService(1, "kernel_call", None, "Kernel", k_id)
 
-    simianEngine.run()
-    simianEngine.exit()
+    # simianEngine.run()
+    # simianEngine.exit()
+    if rank==0:
+        print(f"Runing: {app_and_arg}")
+    for i in range(len(kernels_info)):
+        if rank == i%size:
+            print(f"Kernel {i} is running on rank {rank}")
+            kernel = Kernel(
+                {
+                    "name": "Kernel",
+                    "out": "/dev/null",
+                    "engine": None,
+                    "num": i,  # used
+                }, gpuNode, kernels_info[i])
+            kernel.kernel_call(None, None, None)
 
 
 class GPUNode(object):
